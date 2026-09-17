60261677efbea7607e1c664a2bbe4ee66ad8d288
RohanPhute <rcp5116@gmail.com>
2026-09-17 18:34:49 +0530

    initial server and mongodb setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+const connectDB = require("./config/db")
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials : true
+}));
+
+app.use(express.json());
+
+const startServer = async () => {
+    await connectDB();
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Server started listening on port ${PORT}`)
+    })
+}
+
+
+startServer()
